c8cdd0dc0348e0bde887b563b3b34f6cc297d50b
Joshua Ramat <ramatjoshua@gmail.com>
2023-06-21 18:30:11 -0700

    set up Google OAuth for user authentication

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const keys = require('./config/keys');
+
 const app = express();
 
 const PORT = process.env.PORT || 3301;
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
-
-
+// create new instance of GoogleStrategy
+passport.use(
+  new GoogleStrategy({
+    clientID: keys.googleClientID,
+    clientSecret: keys.googleClientSecret,
+    callbackURL: '/auth/google/callback'
+  }, (accessToken) => {
+    console.log(accessToken);
+  })
+);
 
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
