5890f2a9f1024190878c783e83bd519d92ba1d3f
Joshua Ramat <ramatjoshua@gmail.com>
2023-06-24 18:38:16 -0700

    completed google passport set up

--- index.js
+++ index.js
@@ -13,9 +13,22 @@ passport.use(
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
-  }, (accessToken) => {
-    console.log(accessToken);
+  }, (accessToken, refreshToken, profile, done) => {
+    console.log('access token', accessToken);
+    console.log('refresh token', refreshToken);
+    console.log('profile', profile);
   })
+);
+
+// route handler for google auth
+app.get('/auth/google', 
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  })
+);
+
+app.get('/auth/google/callback',
+  passport.authenticate('google')
 );
 
 app.listen(PORT, () => {
