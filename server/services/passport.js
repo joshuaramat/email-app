7b0c97f46d2e9d3f028849ed7c62bb31ddd471e1
Joshua Ramat <ramatjoshua@gmail.com>
2023-06-24 20:49:12 -0700

    established authentication flow with OAuth and cookies

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,22 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users'); // fetch the users model class
+
+// turn user model instance into id
+passport.serializeUser((user, done) => {
+  done(null, user.id); // user.id is the id in the mongo db
+});
+
+// turn id into mongoose model instance
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user); // user is the user model instance
+    })
+})
 
 // create new instance of GoogleStrategy
 passport.use(
@@ -9,8 +25,18 @@ passport.use(
     clientSecret: keys.googleClientSecret,
     callbackURL: '/auth/google/callback'
   }, (accessToken, refreshToken, profile, done) => {
-    console.log('access token', accessToken);
-    console.log('refresh token', refreshToken);
-    console.log('profile', profile);
+    // check if user already exists
+    User.findOne({ googleId: profile.id })
+      .then((existingUser) => {
+        // if user exists, return user
+        if (existingUser) {
+          done(null, existingUser); 
+        } else {
+          // create new instance of user and save to db
+          new User({ googleId: profile.id })
+            .save()
+            .then(user => done(null, user));
+        }
+      })
   })
 );
